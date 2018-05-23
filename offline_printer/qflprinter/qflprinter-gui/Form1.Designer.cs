ff3b17c2db19b24817695d1266cda416428f3fb7
alexzhang13 <alex.lx.zhang@gmail.com>
2018-05-22 20:11:57 -0400

    Update to QFL Printer (Re-scale and Auto-Resize for smaller devices)

--- offline_printer/qflprinter/qflprinter-gui/Form1.Designer.cs
+++ offline_printer/qflprinter/qflprinter-gui/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.print_tag = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // print_tag
@@ -66,12 +67,24 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 110);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Open Printer Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(486, 373);
+            this.ClientSize = new System.Drawing.Size(482, 500);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.print_tag);
@@ -87,6 +100,7 @@
         private System.Windows.Forms.Button print_tag;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
